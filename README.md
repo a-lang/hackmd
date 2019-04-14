ec54eff8b74257a1c1c11507fda900bad2058a8f
A-Lang <alang.hsu@gmail.com>
2019-04-14 15:31:05 +0800

    Initial update

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# hackmd
-Unofficial desktop client for HackMD.io. It's an open source Electron-based app.
+# HackmdDesktop 
+
+Desktop app for [HackMD][hackmd] packaged with [Electron][electron].
+
+If you don't like the [official app](https://github.com/hackmdio/hackmd-desktop), try out this app.
+
+This Electron-based app is written by the codes which are different from the official app.
+
+Linux
+![](screenshot-linux.png)
+
+Windows
+![](screenshot-win.png)
+
+Mac
+![](screenshot-mac.png)
+
+## Install
+
+The softwares are portable, just unpack the zip file then run it. 
+
+Go to [Release][release] to download the packages for all OS.
+
+## Optional: Create Electron Development
+
+```sh
+git clone https://github.com/a-lang/hackmd.git
+cd hackmd
+npm install
+```
+
+## Optional: Build the distribution files
+
+Build the application for specified platform:
+
+```sh
+npm run start
+npm run dist:linux64
+npm run dist:macos
+npm run dist:win
+```
+
+## License
+Active Record is released under the [MIT license][license]
+
+[hackmd]: https://hackmd.io
+[electron]: http://electron.atom.io
+[license]: https://opensource.org/licenses/MIT
+[release]: https://github.com/a-lang/hackmd/releases
---
+++ menu.js
@@ -0,0 +1,145 @@
+'use strict'
+const app = require('electron').app
+const Menu = require('electron').Menu
+const shell = require('electron').shell
+const name = app.getName()
+const openAboutWindow = require('about-window').default
+const join = require('path').join
+
+
+module.exports = function createMainMenu() {
+    const template = [{
+            label: 'HackMD',
+            submenu: [{
+                label: 'Quit',
+                accelerator: 'Cmd+Q',
+                click: () => {
+                    app.quit()
+                }
+            }]
+        },
+        {
+            label: 'Edit',
+            submenu: [{
+                    label: 'Undo',
+                    accelerator: 'CmdOrCtrl+Z',
+                    role: 'undo'
+                },
+                {
+                    label: 'Redo',
+                    accelerator: 'Shift+CmdOrCtrl+Z',
+                    role: 'redo'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Cut',
+                    accelerator: 'CmdOrCtrl+X',
+                    role: 'cut'
+                },
+                {
+                    label: 'Copy',
+                    accelerator: 'CmdOrCtrl+C',
+                    role: 'copy'
+                },
+                {
+                    label: 'Paste',
+                    accelerator: 'CmdOrCtrl+V',
+                    role: 'paste'
+                },
+                {
+                    label: 'Select All',
+                    accelerator: 'CmdOrCtrl+A',
+                    role: 'selectall'
+                }
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [{
+                    label: 'Reload',
+                    accelerator: 'CmdOrCtrl+R',
+                    click: (item, win) => {
+                        if (win) win.reload()
+                    }
+                },
+                {
+                    label: 'Toggle Full Screen',
+                    accelerator: 'Ctrl+Cmd+F',
+                    click: (item, win) => {
+                        if (win) win.setFullScreen(!win.isFullScreen())
+                    }
+                },
+                {
+                    label: 'Toggle Developer Tools',
+                    accelerator: 'Alt+Cmd+I',
+                    click: (item, win) => {
+                        if (win) win.webContents.toggleDevTools()
+                    }
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'resetzoom'
+                },
+                {
+                    role: 'zoomin'
+                },
+                {
+                    role: 'zoomout'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'togglefullscreen'
+                }
+            ]
+        },
+        {
+            label: 'Window',
+            role: 'window',
+            submenu: [{
+                    label: 'Minimize',
+                    accelerator: 'CmdOrCtrl+M',
+                    role: 'minimize'
+                },
+                {
+                    label: 'Close',
+                    accelerator: 'CmdOrCtrl+W',
+                    role: 'close'
+                }
+            ]
+        },
+        {
+            label: 'Help',
+            role: 'help',
+            submenu: [{
+                    label: 'View on GitHub',
+                    click: () => {
+                        shell.openExternal('https://github.com/a-lang/hackmd')
+                    }
+                },
+                {
+                    label: 'HackMD.io Website ',
+                    click: () => {
+                        shell.openExternal('https://hackmd.io')
+                    }
+                },
+                {
+                    label: 'About ' + name,
+                    click: () => openAboutWindow({
+                        icon_path: join(__dirname, 'build/icon.png'),
+                        license: 'MIT',
+                        description: 'Unofficial HackMD.io Desktop App',
+                    })
+                },
+            ]
+        }
+    ]
+
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+}
